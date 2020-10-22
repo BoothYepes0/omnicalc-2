afab4d47f7ed32f045fc2e4d24b401466a55e67f
yepes0 <yepes0@chicagobooth.edu>
2020-10-22 17:18:41 +0000

    final of omni calc two basic_vF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,50 @@
 class ApplicationController < ActionController::Base
 
+
+#addition
+def addition_form
+  render({ :template => "template/add_form.html.erb"})
+end
+def compute_addition
+    @num1 = params.fetch("num_one").to_f
+    @num2 = params.fetch("num_two").to_f
+    @sum = @num1 + @num2
+    render({ :template => "template/add_results.html.erb" })
+end
+
+#subtraction
+
+def subtract_form
+  render({ :template => "template/subtract_form.html.erb"})
+end
+def compute_subtraction
+    @num1 = params.fetch("num_one").to_f
+    @num2 = params.fetch("num_two").to_f
+    @difference = @num2 - @num1
+    render({ :template => "template/subtract_results.html.erb" })
+end
+#multiplication
+
+def mult_form
+  render({ :template => "template/mult_form.html.erb"})
+end
+def compute_multiplication
+    @num1 = params.fetch("num_one").to_f
+    @num2 = params.fetch("num_two").to_f
+    @product = @num2 * @num1
+    render({ :template => "template/mult_results.html.erb" })
+end
+
+#division
+
+def div_form
+  render({ :template => "template/div_form.html.erb"})
+end
+def compute_division
+    @num1 = params.fetch("num_one").to_f
+    @num2 = params.fetch("num_two").to_f
+    @division = @num1 / @num2
+    render({ :template => "template/div_results.html.erb" })
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get("/add", { :controller =>  "application", :action =>  "addition_form" })
+get("/add/results", { :controller =>  "application", :action =>  "compute_addition" })
+
+get("/subtract", { :controller =>  "application", :action =>  "subtract_form" })
+get("/subtract/results", { :controller =>  "application", :action =>  "compute_subtraction" })
+
+get("/multiply", { :controller =>  "application", :action =>  "mult_form" })
+get("/multiply/results", { :controller =>  "application", :action =>  "compute_multiplication" })
+
+get("/divide", { :controller =>  "application", :action =>  "div_form" })
+get("/divide/results", { :controller =>  "application", :action =>  "compute_division" })
+
 end
